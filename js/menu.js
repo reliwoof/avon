27d25b90c61aec1d4ebe8a57ffc5464d892ee6e4
user09 <>
2017-07-13 11:20:59 +0200

    menu

--- js/menu.js
+++ js/menu.js
@@ -2,16 +2,23 @@ $(function () {
     $("#nav").hide();
     $("#konsultantka, #klient").hide();
 
-    $(".kafelki").click(function(){
+    $(".kafelki2").click(function () {
         var content = $(this).attr("data-name");
         $(".kafelki").fadeOut();
-        $("#nav").fadeIn(function(){
-            $("#"+content).show();
+        $("#nav div[data-name=" + content + "]").children("span").removeClass("menu").addClass("menu2");
+        $("#nav").fadeIn(function () {
+            $("#" + content).fadeIn(200);
         });
+
     });
-    $("#nav span").click(function(){
-      $("#konsultantka, #klient").hide();
-      $("#nav").fadeOut();
-      $(".kafelki").fadeIn();
+    $("#nav div").click(function () {
+        var content = $(this).attr("data-name");
+        if ($("#" + content).css('display') == 'none' && $("#" + content).siblings().css('opacity') == 1) {
+            $(this).children("span").toggleClass("menu menu2");
+            $("#nav div[data-name!=" + content + "]").children("span").removeClass("menu2").addClass("menu");
+            $("#content section[id!=" + content + "]").fadeOut(200, function () {
+                $("#" + content).fadeIn(200);
+            });
+        }
     });
 });
